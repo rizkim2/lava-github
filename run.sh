5f31b2454c4dd9e90ba5eb7a51c36f9f17690418
Newbnode <119142147+rizkim2@users.noreply.github.com>
2024-03-20 10:37:24 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-4527c1e5-f944-453e-8e18-c107f769cc10/)
 
 
 echo "$curl_output" >> Update.tsx
